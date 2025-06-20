a12b6a1f8665af09b075340f411b7746339071ca
iamjoydev <iamjoydevsastri@gmail.com>
2025-06-20 11:34:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: 'D3V‎',
+  botName: "Astro-AI--AstroJD",
+  botOwner: 'DreamW0RLD Tech', // Your name
+  ownerNumber: '919903609484', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
